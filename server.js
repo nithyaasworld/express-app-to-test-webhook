8593e3dfe072c209a1c38899227ef291393ed8f8
Nithya <nithyaasworld@gmail.com>
2022-01-06 07:10:58 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', jsonParser, function(req,res){
+  console.log("\n \n Post request received");
+  console.log(JSON.stringify(req.body));
+  res.send(req.body);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
